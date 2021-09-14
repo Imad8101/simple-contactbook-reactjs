2aad85ff206db0721716d58581cf546f71e8d22d
imad <susuali133@gmail.com>
2021-09-14 11:18:25 +0000

    adding new project files

---
+++ src/components/ContactItem/index.js
@@ -0,0 +1,35 @@
+import './index.css'
+
+const ContactItem = props => {
+  const {contactDetails, toggleIsFavourite} = props
+  const {id, name, mobileNo, isFavorite} = contactDetails
+
+  const onClickFavBtn = () => {
+    toggleIsFavourite(id)
+  }
+
+  const starImgUrl = isFavorite
+    ? 'https://assets.ccbp.in/frontend/react-js/star-filled-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/star-outline-img.png'
+
+  return (
+    <li className="table-row">
+      <div className="table-cell name-column">
+        <p>{name}</p>
+      </div>
+      <hr className="separator" />
+      <div className="table-cell mobile-no-column">
+        <p className="mobile-no-value">{mobileNo}</p>
+        <button
+          type="button"
+          className="favorite-icon-container"
+          onClick={onClickFavBtn}
+        >
+          <img src={starImgUrl} className="favorite-icon" alt="star" />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default ContactItem
